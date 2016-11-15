48e1c7bd7f65a9275ae3ab3d63b20fdd11c6c1a0
Pavel Oliinyk <super.poul@yandex.ru>
2016-11-15 17:52:37 +0200

    formhd fw

---
+++ providence/js/js.js
@@ -0,0 +1,27 @@
+
+  $('.screen').slick({
+  	 autoplay:true,
+  	arrows:false,
+  	slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover:false,
+    speed:1000,
+  })
+
+   $('.slide-face').slick({
+   	 autoplay:true,
+    centerMode: true,
+   	 slidesToShow: 3,
+ // centerPadding: '126px',
+ autoplaySpeed:2000,
+ speed:3000,
+ arrows:true,
+ prevArrow: '.left-button',
+ nextArrow:'.right-button',
+pauseOnHover:false,
+
+
+   })
+
+ 
+
